0dc3ad53ad9ae430fc459add2feb999abf3eb57e
ibrahimambodji <ibrahima.mbodji@infeeeny.com>
2020-09-26 21:34:04 +0200

    feat(IMB-0000): add resource group module

---
+++ Modules/Resource_Group/main.tf
@@ -0,0 +1,14 @@
+#############################################################################################################################
+######                                   TERRAFORM MODULES FOR MICROSOFT AZURE                                           ######
+######  RESOURCE TYPE    :  AZURE RESOURCE GROUP                                                                         ######
+#####   LAST UPDATE      :  26/09/2020                                                                                   ######
+#####   AUTHOR           :  Ibrahima MBODJI                                                                              ######
+#####   VERSION          :  1.0                                                                                          ######
+#####   FILE NAME        :  main.tf                                                                                      ######
+#############################################################################################################################
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
---
+++ Modules/Resource_Group/variables.tf
@@ -0,0 +1,33 @@
+#############################################################################################################################
+######                                   TERRAFORM MODULES FOR MICROSOFT AZURE                                           ######
+######  RESOURCE TYPE    :  AZURE RESOURCE GROUP                                                                         ######
+#####   LAST UPDATE      :  26/09/2020                                                                                   ######
+#####   AUTHOR           :  Ibrahima MBODJI                                                                              ######
+#####   VERSION          :  1.0                                                                                          ######
+#####   FILE NAME        :  variables.tf                                                                                 ######
+#############################################################################################################################
+
+
+variable "rg_name" {
+  type        = string
+  description = "(Required) The Name which should be used for this Resource Group"
+  default     = "rg-imb-hub-dev"
+}
+
+
+variable "rg_location" {
+  type        = string
+  description = "(Required) The Azure Region where the Resource Group should exist."
+  default     = "westeurope"
+}
+
+
+variable "tags" {
+  description = " (Optional) A mapping of tags which should be assigned to the Resource Group."
+  type        = map(string)
+  default = {
+    Terraform_Deployment = "true"
+    Environment          = "dev"
+    Created_By           = "Ibrahima"
+  }
+}
